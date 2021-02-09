c737175db631248e6d4a9d0d3f8a72ddf83cce56
mradamleslie <mr.adam.leslie@gmail.com>
2021-02-09 15:33:09 -0700

    db schema update scores

--- Tailspin.SpaceGame.Database/dbo/Tables/Scores.sql
+++ Tailspin.SpaceGame.Database/dbo/Tables/Scores.sql
@@ -5,5 +5,6 @@
     [score] INT NULL, 
     [gameMode] NVARCHAR(20) NULL, 
     [gameRegion] NVARCHAR(50) NULL, 
+    [gameState] NVARCHAR(50) NULL, 
     CONSTRAINT [FK_Scores_Profiles] FOREIGN KEY ([profileId]) REFERENCES [profiles]([id])
    )

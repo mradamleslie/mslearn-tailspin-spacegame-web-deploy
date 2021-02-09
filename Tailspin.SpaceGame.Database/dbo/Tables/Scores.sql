0db6edba578caa9dae301d22343debdcf065a455
mradamleslie <mr.adam.leslie@gmail.com>
2021-02-09 11:36:56 -0700

    change score game mode width

--- Tailspin.SpaceGame.Database/dbo/Tables/Scores.sql
+++ Tailspin.SpaceGame.Database/dbo/Tables/Scores.sql
@@ -3,7 +3,7 @@
 	[id] INT NOT NULL PRIMARY KEY, 
     [profileId] INT NOT NULL, 
     [score] INT NULL, 
-    [gameMode] NVARCHAR(10) NULL, 
+    [gameMode] NVARCHAR(20) NULL, 
     [gameRegion] NVARCHAR(50) NULL, 
     CONSTRAINT [FK_Scores_Profiles] FOREIGN KEY ([profileId]) REFERENCES [profiles]([id])
    )
